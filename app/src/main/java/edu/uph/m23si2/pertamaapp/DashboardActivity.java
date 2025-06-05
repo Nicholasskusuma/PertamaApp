5b18a5ad2475ff840dfcfc66ff721e64d9af078b
Nicholasskusuma <nicholassk73@gmail.com>
2025-06-05 17:29:45 +0700

    perubahan

--- app/src/main/java/edu/uph/m23si2/pertamaapp/DashboardActivity.java
+++ app/src/main/java/edu/uph/m23si2/pertamaapp/DashboardActivity.java
@@ -1,6 +1,9 @@
 package edu.uph.m23si2.pertamaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DashboardActivity extends AppCompatActivity {
-
+    LinearLayout llyProfil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,18 @@ public class DashboardActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        llyProfil= findViewById(R.id.llyProfil);
+        llyProfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toProfil();
+            }
+        });
+    }
+    public void toProfil(){
+        Intent intent = new Intent(this,ProfilActivity.class);
+        intent.putExtra("nama","Satria");
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/edu/uph/m23si2/pertamaapp/ProfilActivity.java
@@ -0,0 +1,73 @@
+package edu.uph.m23si2.pertamaapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ProfilActivity extends AppCompatActivity {
+    Button btnSubmit;
+    EditText edtNama, edtEmail, edtProdi;
+    TextView txvHasil;
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnSubmit = findViewById(R.id.btnSubmit);
+        edtNama = findViewById(R.id.edtNama);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtProdi = findViewById(R.id.edtProdi);
+        txvHasil = findViewById(R.id.txtvHasil);
+
+        edtNama.setText(getIntent().getStringExtra("nama"));
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Menampilkan hasil di txvHasil
+                String nama = edtNama.getText().toString();
+                String email = edtEmail.getText().toString();
+                String prodi = edtProdi.getText().toString();
+                txvHasil.setText(nama + "\n"+ email + "\nProgram Studi" + prodi + "\n" +
+                        getNamaFakultas(prodi));
+            }
+        });
+    }
+
+    String getNamaFakultas(String prodi){
+        String namaProdi = prodi.toLowerCase();
+        if(namaProdi.equals("sistem informasi") || namaProdi.equals("informatika")){
+            return "Fakultas Teknologi Informasi";
+        }
+        else if(namaProdi.equals("hukum") || namaProdi.equals("law")){
+            return "Fakultas Hukum";
+        }
+        else if(namaProdi.equals("akuntansi") || namaProdi.equals("manajemen")||
+                namaProdi.equals("perhotelan")){
+            return "Fakultas Ekonomi dan Bisnis";
+        }
+        else
+            return "Fakultas Tidak di Temukan";
+    }
+}
